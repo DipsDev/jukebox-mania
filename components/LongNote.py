1764de605ce67aa760be7ec377b75cca6386a0e7
1Dips <ido.geva.b@gmail.com>
2023-01-21 12:47:39 +0200

    Testing long notes

--- components/LongNote.py
+++ components/LongNote.py
@@ -1,8 +1,6 @@
 import pygame.transform
 
-import game
 from components.Note import Note
-from utils import Utils
 
 
 class LongNote(Note):
@@ -11,17 +9,29 @@ class LongNote(Note):
         self._height = height
         self._original_height = height
         self._holding_beats = holding_beats
+        self._started = False
         self._holding_time_counter = 0
+        self.__middle_long_tile = pygame.image. \
+            load(f"./assets/notes/long_notes/{adjacent_key.get_color()}_tile/{adjacent_key.get_color()}_tile_mid")
+        self.__top_long_tile = pygame.image. \
+            load(f"./assets/notes/long_notes/{adjacent_key.get_color()}_tile/{adjacent_key.get_color()}_tile_top")
+        self.__bot_long_tile = pygame.image. \
+            load(f"./assets/notes/long_notes/{adjacent_key.get_color()}_tile/{adjacent_key.get_color()}_tile_bot")
         self.resize()
 
     def resize(self):
         self._original_height = self._sprite.get_height()
-        self._sprite = pygame.transform.scale(self._sprite, (self._sprite.get_width(),
-                                                             self._height + self._original_height))
+        self.__middle_long_tile = pygame.transform.smoothscale(self.__middle_long_tile,
+                                                               (self.__middle_long_tile.get_width(),
+                                                                round(self._height + self._original_height)))
 
     def render(self, surface: pygame.Surface):
-        surface.blit(self._sprite, self._sprite.get_rect(
-            midbottom=(self._pos[0], self._pos[1] + self._original_height / 2)))
+        pos = (self._pos[0], self._pos[1] + self._original_height / 2)
+        if not self._started:
+            surface.blit(self.__bot_long_tile, self.__bot_long_tile.get_rect(midbottom=pos))
+            self._started = True
+            return
+        surface.blit(self.__middle_long_tile, self.__middle_long_tile.get_rect(midbottom=(pos[0], pos[1] + self.__bot_long_tile.get_height())))
 
     def move(self):
         # Implement Logic!
